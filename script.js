a7a961f260a3510fe115539e07779a21c771cbde
Katerina <esidor27@ya.ru>
2024-08-28 16:56:11 +0300

    first level

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const currentDate = new Date();
+const targetDate = new Date('2024-09-01T23:59:59');
+const difference = targetDate - currentDate;
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const difference = targetDate - currentTime;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (difference < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerText = "Время вышло!";
+      }
+}
+
+const interval = setInterval(updateCountdown, 1000);  
